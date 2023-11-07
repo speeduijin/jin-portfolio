9959f2034bd316d2c3c59f06e0558b61166dc201
Jeong Euijin <speeduijin91@gmail.com>
2023-11-07 17:59:18 +0900

    feat(DB): Connect to PostgreSQL DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import {
+  ENV_DB_HOST_KEY,
+  ENV_DB_PORT_KEY,
+  ENV_DB_USERNAME_KEY,
+  ENV_DB_PASSWORD_KEY,
+  ENV_DB_DATABASE_KEY,
+} from './common/const/env-keys.const';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env[ENV_DB_HOST_KEY],
+      port: parseInt(process.env[ENV_DB_PORT_KEY]),
+      username: process.env[ENV_DB_USERNAME_KEY],
+      password: process.env[ENV_DB_PASSWORD_KEY],
+      database: process.env[ENV_DB_DATABASE_KEY],
+      entities: [],
+      synchronize: true,
+    }),
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/const/env-keys.const.ts
@@ -0,0 +1,5 @@
+export const ENV_DB_HOST_KEY = 'DB_HOST';
+export const ENV_DB_PORT_KEY = 'DB_PORT';
+export const ENV_DB_USERNAME_KEY = 'DB_USERNAME';
+export const ENV_DB_PASSWORD_KEY = 'DB_PASSWORD';
+export const ENV_DB_DATABASE_KEY = 'DB_DATABASE';
